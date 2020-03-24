a07d1457190134d5c24db4a987cb0d8ff2c58e39
Henri <Henri@DESKTOP-V8BMQTE.homenet.telecomitalia.it>
2020-03-24 15:54:17 +0100

    salvataggio di prova

---
+++ src/main/java/it/polito/tdp/libretto/model/Libretto.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package it.polito.tdp.libretto.model;
+
+/**
+ * @author Henri
+ *
+ */
+public class Libretto {
+
+}
---
+++ src/main/java/it/polito/tdp/libretto/model/TestLibretto.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package it.polito.tdp.libretto.model;
+
+/**
+ * @author Henri
+ *
+ */
+public class TestLibretto {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
